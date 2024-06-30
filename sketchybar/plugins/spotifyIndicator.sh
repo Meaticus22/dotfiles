34a366102761445415710285b210cdc468853661
CyberMeaticus <117058929+Meaticus22@users.noreply.github.com>
2024-06-30 02:02:08 -0400

    Update spotifyIndicator.sh

--- sketchybar/plugins/spotifyIndicator.sh
+++ sketchybar/plugins/spotifyIndicator.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
+
+# Define the item name used in Sketchybar
+ITEM_NAME="spotifyIndicator"
+
+# Check if Spotify is running
 RUNNING=$(osascript -e 'if application "Spotify" is running then return 0')
 if [ "$RUNNING" == "" ]; then
   RUNNING=1
 fi
+
 PLAYING=1
 TRACK=""
 ALBUM=""
 ARTIST=""
-if [ "$(osascript -e 'if application "Spotify" is running then tell application "Spotify" to get player state')" == "playing" ]; then
+
+# Get Spotify player state
+PLAYER_STATE=$(osascript -e 'if application "Spotify" is running then tell application "Spotify" to get player state')
+
+if [ "$PLAYER_STATE" == "playing" ]; then
   PLAYING=0
   TRACK=$(osascript -e 'tell application "Spotify" to get name of current track')
   ARTIST=$(osascript -e 'tell application "Spotify" to get artist of current track')
   ALBUM=$(osascript -e 'tell application "Spotify" to get album of current track')
-  sketchybar -m --set $NAME drawing=on
-fi
-if [ $RUNNING -eq 0 ] && [ $PLAYING -eq 0 ]; then
-  if [ "$ARTIST" == "" ]; then
-    sketchybar -m --set $NAME label="$TRACK - $ALBUM"
-  else
-    sketchybar -m --set $NAME label="$TRACK - $ARTIST"
-  fi
+  
+  # Update Sketchybar item with track and artist
+  sketchybar -m --set $ITEM_NAME drawing=on label="$TRACK - $ARTIST"
 else
-  sketchybar -m --set $NAME drawing=off
-  sketchybar -m --set $NAME label=""
+  # Turn off drawing and clear label if Spotify is not playing
+  sketchybar -m --set $ITEM_NAME drawing=off label=""
 fi
+
